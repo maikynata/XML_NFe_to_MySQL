546f42c9f2e1491998c7dbd00acb097b3f7cf58c
Maiky Nata <maikynata25@gmail.com>
2019-04-19 11:48:58 -0300

    XML_NFe_to_MySQL

---
+++ XML_NFe_to_MySQL.py
@@ -0,0 +1,24 @@
+from nfelib.v4_00 import leiauteNFe_sub as parser
+import mysql.connector
+
+nota = parser.parse('novo.xml')
+
+db = mysql.connector.Connect(host = '127.0.0.1', user = 'root', password ='' , database = 'test')
+    query = "SELECT * FROM test.teste LIMIT 10"
+        cur = db.cursor()
+        cur.execute(query)
+        records = cur.fetchall()
+print(records)
+
+
+
+print(nota.infNFe.emit.CNPJ)
+
+print(nota.infNFe.det[0].prod.cEAN)
+
+nota.infNFe.det[0]
+
+
+
+# print do nó inteiro
+# nota.infNFe.emit.export(sys.stdout, 0)
