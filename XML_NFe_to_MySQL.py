0ab73bf652df44dcb837c143dd3172247b552ea2
Maiky Nata <maikynata25@gmail.com>
2019-04-20 11:59:49 -0300

    simple tests folder

--- XML_NFe_to_MySQL.py
+++ XML_NFe_to_MySQL.py
@@ -1,15 +1,14 @@
 from nfelib.v4_00 import leiauteNFe_sub as parser
 import mysql.connector
 
-nota = parser.parse('novo.xml')
+nfe = parser.parse('tests\\novo.xml')
 
-db = mysql.connector.Connect(host = '127.0.0.1', user = 'root', password ='' , database = 'test')
-    query = "SELECT * FROM test.teste LIMIT 10"
-        cur = db.cursor()
-        cur.execute(query)
-        records = cur.fetchall()
+db = mysql.connector.Connect(host='127.0.0.1', user='root', password='', database='test')
+query = "SELECT * FROM test.teste LIMIT 10"
+cur = db.cursor()
+cur.execute(query)
+records = cur.fetchall()
 print(records)
-
 
 # teste 123
 
@@ -18,8 +17,6 @@ print(nota.infNFe.emit.CNPJ)
 print(nota.infNFe.det[0].prod.cEAN)
 
 nota.infNFe.det[0]
-
-
 
 # print do nó inteiro
 # nota.infNFe.emit.export(sys.stdout, 0)
