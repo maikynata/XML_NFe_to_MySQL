4824a9df3f9d8c982f70a7600b72a79288245a8a
Maiky Nata <maikynata25@gmail.com>
2019-06-20 11:14:44 -0300

    NFCe field names and insert updates

--- XML_NFCe.py
+++ XML_NFCe.py
@@ -3,30 +3,23 @@ import mysql.connector
 import os
 
 # path = 'C:\\Users\\Avell\\Desktop\\tests'
-# path = 'C:\\Users\\Avell\\Desktop\\nfce'
+path = 'C:\\Users\\Avell\\Desktop\\nfce'
 #
-# for filename in os.listdir(path):
-#     nfce = parser.parse('%s\\%s' % (path, filename))
+for filename in os.listdir(path):
+    nfce = parser.parse('%s\\%s' % (path, filename))
 
 # Manual path to test:
-nfce = parser.parse('tests\\teste.xml')
+# nfce = parser.parse('tests\\nfce-dist.xml')
 
     def insertXML(newlist):
         try:
             connection = mysql.connector.connect(host='127.0.0.1', user='root', password='', database='xml')
 
-            sql_insert_query = """ INSERT INTO `nfce022019` VALUES (NULL,%s,NULL,NULL,NULL,NULL,%s,NULL,%s,%s,%s,%s,%s,%s,%s,
-            %s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,'RP_INFO_1.0.9.9',NULL,%s,'Goias Comercio De Cosmeticos Ltda',
-            NULL,'Av. Central N. 577 Qd. 197 Lt. 03',577,'Setor Empresarial',5208707,'Goiania','GO',74583350,1058,'Brasil',
-            6235866377,105620947,3,NULL,17871449000985,'Goias Comercio De Cosmeticos Ltda',NULL,'Rod. Br-040 Km 12 Gleba f Lojas 065/066',
-            000,'Parque Esplanada 3',5221858,'Valparaiso De Goias','GO',72876902,1058,'Brasil',6136270180,1,105650323,NULL,%s,NULL,%s,
-            %s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,NULL,NULL,NULL,0,20,3,41.6700,1.32,12.0000,
-            0.16,NULL,NULL,49,0.00,0.0000,0.00,NULL,NULL,49,0.00,0.0000,0.00,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,
-            NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,
-            NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,
-            NULL,NULL,NULL,NULL,NULL,NULL,NULL)"""
-            #insert_tuple = (versao,Id,cUF,cNF,natOp,mod,serie,nNF,dhEmi,dhSaiEnt,tpNF,idDest,cMunFG,tpImp,tpEmis,
-    #cDV,tpAmb,finnfce,indFinal,indPres, procEmi)
+            sql_insert_query = """ INSERT INTO `nfce_teste` VALUES (%s,%s,
+            %s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,
+            'Goias Comercio De Cosmeticos Ltda',NULL,NULL,'Av. Central N. 577 Qd. 197 Lt. 03',577,'Setor Empresarial',5208707,'Goiania','GO',74583350,1058,'Brasil',6235866377,105620947,3,
+            %s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"""
+
             insert_tuple = newlist
             cursor = connection.cursor(prepared=True)
 
@@ -49,7 +42,7 @@ nfce = parser.parse('tests\\teste.xml')
     for index in range(len(nfce.infNFe.det)):
         newlist.append((
             nfce.infNFe.versao,
-            nfce.infNFe.Id,
+            nfce.infNFe.Id[3:],
             nfce.infNFe.ide.cUF,
             nfce.infNFe.ide.cNF,
             nfce.infNFe.ide.natOp,
@@ -57,7 +50,6 @@ nfce = parser.parse('tests\\teste.xml')
             nfce.infNFe.ide.serie,
             nfce.infNFe.ide.nNF,
             nfce.infNFe.ide.dhEmi,
-            nfce.infNFe.ide.dhSaiEnt,
             nfce.infNFe.ide.tpNF,
             nfce.infNFe.ide.idDest,
             nfce.infNFe.ide.cMunFG,
